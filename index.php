82b861557df15eb3fc72c724495af87751001b44
Александра <wirvorfi@gmail.com>
2026-04-07 12:39:06 +0300

    Шаг 1.3

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$file = fopen("test.txt", "r");
-$text = fgets($file);
-fclose($file);
-
-echo "Содержимое файла: " . $text;
+if (rename("test.txt", "mir.txt")) {
+    echo "Файл test.txt переименован в mir.txt";
+} else {
+    echo "Ошибка при переименовании файла";
+}
 ?>
