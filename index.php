8947f42b5bccbb5f04938e2ea4e6afaa26ef56c4
Александра <wirvorfi@gmail.com>
2026-04-07 14:26:28 +0300

    Шаг 2.8

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php
-$date1 = $_POST['date1'];
-$date2 = $_POST['date2'];
-
-if ($date1 > $date2) {
-    echo $date1;
-}
-else {
-    echo $date2;
-}
+$date = '2025-12-31';
+echo date('d-m-Y', strtotime($date));
 ?>
-
-<form method="post">
-    <input type="text" name="date1" placeholder="гггг-мм-дд">
-    <input type="text" name="date2" placeholder="гггг-мм-дд">
-    <button type="submit">Сравнить</button>
-</form>
