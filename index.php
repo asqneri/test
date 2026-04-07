054e27466ba84ad4832a9c65573f31f46ab03a1c
Александра <wirvorfi@gmail.com>
2026-04-07 12:53:29 +0300

    Шаг 2.1

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo file_exists("folder/world.txt") ? "world.txt существует<br>" : "world.txt не существует<br>";
+mkdir("test");
 
-echo file_exists("folder/mir.txt") ? "mir.txt существует<br>" : "mir.txt не существует<br>";
+echo "Папка test создана";
 ?>
