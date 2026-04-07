c203661e111d18a8a03a8ae712401c03bf71178f
Александра <wirvorfi@gmail.com>
2026-04-07 12:40:51 +0300

    Шаг 1.4

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-if (rename("test.txt", "mir.txt")) {
-    echo "Файл test.txt переименован в mir.txt";
-} else {
-    echo "Ошибка при переименовании файла";
-}
+mkdir("folder");
+rename("mir.txt", "folder/mir.txt");
+
+echo "Папка folder создана, файл mir.txt перемещён в нее";
 ?>
