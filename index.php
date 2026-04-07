6c85f2dededede79bcb7f85dd007ba500deb2e1c
Александра <wirvorfi@gmail.com>
2026-04-07 15:09:43 +0300

    Все шаги вместе

--- index.php
+++ index.php
@@ -13,6 +13,16 @@ class Rabotnik
         $this->salary = $salary;
     }
 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getSalary()
+    {
+        return $this->salary;
+    }
+
     private function checkAge($age)
     {
         if ($age >= 18) {
@@ -27,7 +37,7 @@ class Rabotnik
         if ($this->checkAge($age) == true) {
             $this->age = $age;
         } else {
-            echo "Вам работать в нашей компании еще рано";
+            echo "Вам работать в нашей компании еще рано<br>";
         }
     }
 
@@ -38,10 +48,17 @@ class Rabotnik
 }
 
 $worker1 = new Rabotnik("Анна", 25, 30000);
+$worker2 = new Rabotnik("Иван", 30, 40000);
+
+echo "Имя первого работника: " . $worker1->getName() . "<br>";
+echo "Имя второго работника: " . $worker2->getName() . "<br>";
+
+echo "Сумма зарплат: " . ($worker1->getSalary() + $worker2->getSalary()) . "<br>";
+echo "Сумма возрастов: " . ($worker1->getAge() + $worker2->getAge()) . "<br>";
 
 $worker1->setAge(22);
-echo $worker1->getAge() . "<br>";
+echo "Новый возраст первого работника: " . $worker1->getAge() . "<br>";
 
-$worker1->setAge(15);
+$worker2->setAge(16);
 
 ?>
