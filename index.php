1d2d6d5e715d8bb006f7b7bf424e3f68cd52c2a2
Александра <wirvorfi@gmail.com>
2026-04-08 15:51:33 +0300

    Добавили класс BlogPage с наследованием и немного поменяли класс Page

--- index.php
+++ index.php
@@ -7,8 +7,18 @@ class Page
 
     public function __construct()
     {
-        $this->name = "page";
-        $this->template = "<div><p>It is a default page</p></div>";
+        $this->name = "music";
+        $this->template = "
+        <div>
+            <iframe style='border-radius:12px'
+                    width='100%'
+                    src='https://open.spotify.com/embed/track/4l3z5vr4sgHuY6IMWzsgnN?si=5bd09bb21f404bf1'
+                    height='152'
+                    frameBorder='0'
+                    allowfullscreen=''
+                    allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'>
+                    </iframe>
+        </div>";
     }
 
     public function render(): void
@@ -17,7 +27,37 @@ class Page
     }
 }
 
+class BlogPage extends Page
+{
+    public function __construct()
+    {
+        $this->name = "blog";
+        $this->template = "
+        <div style='display: flex; justify-content: space-evenly;'>
+            <div style='border:1px solid #ccc; width:300px; margin-bottom:20px; padding:10px;'>
+                <img src='https://img.championat.com/i/o/u/17588865071944996032.jpg' style='width:100%;'>
+                <h3>Лучшие аниме 2026 года</h3>
+                <p>Подборка самых ожидаемых и популярных аниме 2026 года.</p>
+                <p>Аниме</p>
+            </div>
+
+            <div style='border:1px solid #ccc; width:300px; margin-bottom:20px; padding:10px;'>
+                <img src='https://static.rustore.ru/imgproxy/-0AZtWxSfoTw9HwQAU2MUzqZV7zSZH-EfFg6Et2lY6k/plain/https://static.rustore.ru/rustore-strapi/9/01_Kak_igrat_v_Genshin_Impact_a44a38bec5.webp@webp' style='width:100%;'>
+                <h3>Лучшие сборки Genshin Impact 2026</h3>
+                <p>Актуальные билды персонажей и лучшие артефакты.</p>
+                <p>Genshin</p>
+            </div>
+        </div>
+        ";
+    }
+}
+
 $page = new Page();
 $page->render();
+
+echo "<br><br>";
+
+$blog = new BlogPage();
+$blog->render();
 
 ?>
